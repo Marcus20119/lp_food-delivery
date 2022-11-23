724745dfc9e5120e288816965ec9cb458cd3f6e9
duchuy <dhuy221001@gmail.com>
2022-11-23 14:59:09 +0700

    update

--- src/components/Frame7/index.jsx
+++ src/components/Frame7/index.jsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components'
+import Contact from './Contact'
+import Information from './Information'
+const StyledFrame7 = styled.div`
+  width: 1300px;
+  display: grid;
+  margin: 0 auto;
+  grid-template-columns: 500px auto;
+  margin-bottom: 100px;
+  grid-gap: 50px;
+`
 const Frame7 = () => {
-  return <div></div>;
+  return <StyledFrame7>
+    <Contact></Contact>
+    <Information></Information>
+  </StyledFrame7>;
 };
 
 export default Frame7;
